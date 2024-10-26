f0bb2103e69c87547f9e027f21087986b5358318
anton k <anton.kolomiets.91@gmail.com>
2024-10-26 23:24:19 +0300

    fix in test_log_event_incorrect_log_level file

--- all_lessons/lesson_11/test_log_event_negative.py
+++ all_lessons/lesson_11/test_log_event_negative.py
@@ -8,24 +8,24 @@ from all_lessons.lesson_11.log_event_function import log_event
     ('Joe', 'success', 'ERROR'),
     ('Inna', 'expired', 'INFO'),
     ('Ross', 'failed', 'WARNING'),
-    ('Ira', None, 'INFO')
+    ('Ira', None, 'INFO'),
+    (123, 21312, 'WARNING')
 ])
 
 def test_log_event_incorrect_log_level(user_name, status, log_level):
 
-    with pytest.raises(AssertionError):
 
-        log_event(user_name, status)
+    log_event(user_name, status)
 
-        with open('login_system.log') as log_file:
-            log_lines = log_file.readlines()
+    with open('login_system.log') as log_file:
+        log_lines = log_file.readlines()
 
-        assert len(log_lines) > 0
+    assert len(log_lines) > 0
 
-        last_login = log_lines[-1].strip()
+    last_login = log_lines[-1].strip()
 
-        expected_result = f'Login event - Username: {user_name}, Status: {status} - {log_level}'
+    expected_result = f'Login event - Username: {user_name}, Status: {status} - {log_level}'
 
-        assert expected_result in last_login
+    assert expected_result not in last_login
 
 
