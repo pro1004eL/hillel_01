7f492de16f5d9c673ae1cae864d5754c4b93f5f9
anton k <anton.kolomiets.91@gmail.com>
2024-11-05 11:20:43 +0200

    fix function read_cdv_file for homework13_1_csv

--- all_lessons/lesson_13/homework13_csv/homework13_1_csv.py
+++ all_lessons/lesson_13/homework13_csv/homework13_1_csv.py
@@ -1,27 +1,23 @@
 import csv
 
 
-unique_rows = set()
-
 def read_csv_file(file):
     with open(file) as f:
         reader = list(csv.reader(f))
-
-        global headers
-        headers = reader[0]
-        reader_rows = reader[1:]
-
-        for row in reader_rows:
-            unique_rows.add(tuple(row))
+        header = reader[0]
+        return header, reader[1:]
 
 
-read_csv_file('r-m-c.csv')
-read_csv_file('rmc.csv')
+unique_rows = set()
+header, data = read_csv_file('r-m-c.csv')
+unique_rows.update(set(tuple(row) for row in data))
+header_2, data_2 = read_csv_file('rmc.csv')
+unique_rows.update(set(tuple(row) for row in data_2))
 
 
 with open('file_without_duplicates.csv', 'w', newline='') as f_no_d:
     writer = csv.writer(f_no_d)
-    writer.writerow(headers)
+    writer.writerow(header)
     for row in unique_rows:
         writer.writerow(row)
 
